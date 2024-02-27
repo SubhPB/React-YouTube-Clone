efcd8f4ba795b0d1656f5b1e301cff11a4f7868d
SubhPB <harvirbuttar2917@gmail.com>
2024-02-26 22:02:38 -0800

    added custom useEffect hook for fetching data

---
+++ src/src-utils/useFetch.tsx
@@ -0,0 +1,39 @@
+/* -- BYIMAAN -> THE FUTURE -- */
+
+import { obj } from "./dataTypes";
+import { useEffect, useState } from "react";
+import { fetchData } from "./api";
+
+interface UseFetchProp<T> {
+    data: T | null,
+    error: string | null,
+    isLoading: boolean,
+};
+
+export default function useFetch<T>(url: string): UseFetchProp<T> {
+
+    const [data, setData] = useState<T | null>(null);
+    const [isLoading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect ( function task(){
+
+        fetchData(url).then( function success(data){
+            setData(data);   
+        }).catch( function fail(error){
+            setError(error);
+        }).finally( function final(){
+            setLoading(false);
+        });
+
+        return function cleanUpFunc() {
+            setLoading(true);
+            setError(null);
+            setData(null);
+        };
+
+    }, [url]);
+
+    return {data, error, isLoading}
+
+};
