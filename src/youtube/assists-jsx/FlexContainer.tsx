a91a410a754b7ca17b6cad0f8e789bff816e062a
SubhPB <harvirbuttar2917@gmail.com>
2024-02-27 20:46:18 -0800

    added loading functionaility to flex container

--- src/youtube/assists-jsx/FlexContainer.tsx
+++ src/youtube/assists-jsx/FlexContainer.tsx
@@ -3,12 +3,12 @@
 import React from 'react';
 import { CmnProps } from './Assists';
 
-const FlexContainer: React.FC<CmnProps> = ({className='flex w-full h-full flex-wrap flex-shrink-0 ', xtraCss='', children}) => {
+const FlexContainer: React.FC<CmnProps<any>> = ({className='flex w-full h-full flex-wrap flex-shrink-0 ', xtraCss='', children}) => {
   return (
     <div className={`flex-container ${className} ${xtraCss}`}>
       {children}
     </div>
   )
-}
+};
 
 export default FlexContainer
