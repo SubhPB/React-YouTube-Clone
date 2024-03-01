b388f5f98c9e9fa5323a05439ac7282fd40453e3
SubhPB <harvirbuttar2917@gmail.com>
2024-03-01 09:48:01 -0800

    n/a

--- src/src-utils/dataTypes.tsx
+++ src/src-utils/dataTypes.tsx
@@ -1,7 +1,17 @@
 /* -- BYIMAAN -> THE FUTURE -- */
 
+import {ReactNode} from "react";
+
 export interface obj<T>{
     [key: string]: T,
+};
+
+export interface VideoCardElem<T> {
+    className ?: string;
+    xtraCss ?: string;
+    source ?: T | undefined;
+    children ?: ReactNode;
+    loading ?: boolean;
 };
 
 export type Func<Params extends any[], R=void> = (...args: Params) => R;
