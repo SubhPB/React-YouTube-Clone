4eb8f9aa97e45e8ee00501463c37dbe639568445
SubhPB <harvirbuttar2917@gmail.com>
2024-03-03 18:22:27 -0800

    for diplaying genres as related content added Mini-Header component

--- src/youtube/components-jsx/MiniHeader.tsx
+++ src/youtube/components-jsx/MiniHeader.tsx
@@ -2,31 +2,38 @@
 
 import React from 'react';
 import { CmnProps } from '../assists-jsx/Assists';
+import { useNavigate } from 'react-router-dom';
 
 function MiniHeader({className=' ', xtraCss='', children} : CmnProps<any>) {
   return (
     <div className={`mini-header ${className} ${xtraCss}`}>
-      
+      {children}
     </div>
   )
 };
 
-export const Genre: React.FC<CmnProps<string>> = ({className='p-4 font-semibold text-lg', xtraCss='', source}) => {
+export const Genre: React.FC<CmnProps<string>> = ({className='py-2 px-3 font-semibold text-sm bg-zinc-600 rounded-[10px] cursor-pointer', xtraCss='', source='Byimaan'}) => {
+
+    const navigate = useNavigate();
 
     if (!source) return <></>;
 
+    const handleClick = () => { navigate('/query/ ' + source ) };
+
     return (
-        <div className={`genre ${className} ${xtraCss}`}>{source}</div>
+        <div onClick={handleClick} className={`genre ${className} ${xtraCss} whitespace-nowrap`}>{source}</div>
     );
 };
 
-export const Genres: React.FC<CmnProps<string[]>> = ({className=' ', xtraCss='', source}) => {
+export const Genres: React.FC<CmnProps<string[]>> = ({className='fixed z-[5] flex bg-zinc-900 p-2 gap-2 flex-shrink-0 w-full overflow-x-scroll scrollbar-hide ', xtraCss='', source}) => {
 
-    if (!source) return <></>;
+    if (!source || source?.length === 0 ) return <></>;
 
     return (
         <div className={`genres ${className} ${xtraCss}`}>
-
+            {
+                source?.map( (genre, index) => <Genre key={index} source={genre}/>)
+            }
         </div>
     )
 
