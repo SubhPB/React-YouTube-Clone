b08f9383e0361264410d074dcf1a5647006a09eb
SubhPB <harvirbuttar2917@gmail.com>
2024-03-05 09:07:42 -0800

    style (Tailwind) changes in this component

--- src/youtube/assists-jsx/FlexContainer.tsx
+++ src/youtube/assists-jsx/FlexContainer.tsx
@@ -1,14 +1,27 @@
 /* -- BYIMAAN -> THE FUTURE -- */
 
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import { CmnProps } from './Assists';
 
-const FlexContainer: React.FC<CmnProps<any>> = ({className='flex w-full h-full flex-wrap flex-shrink-0 ', xtraCss='', children}) => {
+
+function FlexContainer({ className = 'flex w-full h-full flex-wrap flex-shrink-0', xtraCss = '', children, componentDidMount= () => {}, componentWillUnMount = () => {} }: CmnProps<any>): JSX.Element {
+
+  const myRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+
+    componentDidMount(myRef.current);
+    
+    return () => {
+      componentWillUnMount(myRef.current); 
+    }
+
+  }, [])
   return (
-    <div className={`flex-container ${className} ${xtraCss}`}>
+    <div ref={myRef} className={`flex-container ${className} ${xtraCss}`}>
       {children}
     </div>
-  )
+  );
 };
 
 export default FlexContainer
