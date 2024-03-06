1ce05806b27722f85c406851f0687c74fa9adb17
SubhPB <harvirbuttar2917@gmail.com>
2024-03-06 09:44:07 -0800

    made changes in watch.tsx for different functionalities for different screen sizes

--- src/youtube/pages-jsx/watch-YT/VideoDisplay.tsx
+++ src/youtube/pages-jsx/watch-YT/VideoDisplay.tsx
@@ -5,6 +5,9 @@ import { vidCardTS } from '../../../src-utils/dataTypes';
 
 
 function VideoDisplay({className='', xtraCss='', source, isLoading }: vidCardTS): React.ReactElement {
+
+  if (isLoading) return <div className={`load-vid-display ${className} ${xtraCss}`}> <div className=' h-full w-full bg-zinc-600 rounded-lg'/></div>
+
   return (
     <div className={`vid-diplay ${className} ${xtraCss} `}>
       
--- src/youtube/pages-jsx/watch-YT/Watch.tsx
+++ src/youtube/pages-jsx/watch-YT/Watch.tsx
@@ -1,9 +1,13 @@
 /* -- BYIMAAN -> THE FUTURE -- */
 
-import React, {useEffect} from 'react'
-import { CmnProps } from '../../assists-jsx/Assists'
-import { useNavigate, useParams } from 'react-router-dom'
-import { render } from '@testing-library/react';
+import React, {useEffect} from 'react';
+import { CmnProps } from '../../assists-jsx/Assists';
+import { useNavigate, useParams } from 'react-router-dom';
+import VideoContent from './Cinema';
+import RelatedContent from './RelatedContent';
+import PublisherData from './PublisherData';
+import demoVideoDetail from '../../assists-jsx/videoDetailDemo.json';
+import { VideoDetailApiResponse } from '../../assists-jsx/videoDetailTs';
 
 const Watch: React.FC<CmnProps<any>> = ({className='', xtraCss='', callBackFunc= function(val:boolean){}}) => {
 
@@ -27,9 +31,16 @@ const Watch: React.FC<CmnProps<any>> = ({className='', xtraCss='', callBackFunc=
         return <></>;
     };
 
+    const publishData = {source: demoVideoDetail as VideoDetailApiResponse , isLoading: false}
+  
+
+    // 2 components - Cinema & RelatedVids
+
+    // watch-yt flex-grow-[1] max-h-dvh overflow-y-scroll lg:scrollbar-cstm bg-zinc-900 flex justify-between flex-col lg:flex-row
     return (
-        <div className={`watch-yt flex-grow-[1] max-h-dvh overflow-y-scroll lg:scrollbar-cstm bg-zinc-600 ${className} ${xtraCss}`}>
-            This is the video id {videoId}
+        <div className={`watch-yt flex-grow-[1] max-h-dvh overflow-y-scroll lg:scrollbar-cstm bg-zinc-900 flex justify-between flex-col lg:flex-row ${className} ${xtraCss}`}>
+            <VideoContent className='min-h-[30%] lg:h-full w-full lg:w-[65%] flex flex-col px-2' children={<PublisherData xtraCss='hidden lg:flex' source={publishData.source} isLoading={publishData.isLoading}/>}  />
+            <RelatedContent className='flex-grow-[1] max-h-dvh overflow-y-scroll lg:scrollbar-cstm lg:h-full w-full lg:w-[34%]' children={<PublisherData className=' lg:hidden' source={publishData.source} isLoading={publishData.isLoading} />}/>
         </div>
     );
 };
