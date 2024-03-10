b6858b187e19ab09bc2ace54c6559fd9406a06d7
SubhPB <harvirbuttar2917@gmail.com>
2024-03-10 04:21:49 -0700

    N/A

--- src/youtube/pages-jsx/watch-YT/Watch.tsx
+++ src/youtube/pages-jsx/watch-YT/Watch.tsx
@@ -8,11 +8,24 @@ import RelatedContent from './RelatedContent';
 import PublisherData from './PublisherData';
 import demoVideoDetail from '../../assists-jsx/videoDetailDemo.json';
 import { VideoDetailApiResponse } from '../../assists-jsx/videoDetailTs';
+import { Routes, Route } from 'react-router-dom';
+import useFetch from '../../../src-utils/useFetch';
+import VidDescription from '../Description-YT/VidDescription';
+
+interface PublishTS {
+    data : VideoDetailApiResponse,
+    isLoading : boolean
+}
 
 const Watch: React.FC<CmnProps<any>> = ({className='', xtraCss='', callBackFunc= function(val:boolean){}}) => {
 
     const {videoId} = useParams();
     const navigate = useNavigate();
+
+    const _data = demoVideoDetail as VideoDetailApiResponse
+
+    // const publishData = useFetch<VideoDetailApiResponse>(`video/details/?id=${videoId}`);
+    const publishData: PublishTS = {data: _data, isLoading: false}
 
     useEffect( function componentDidMount(){
 
@@ -25,34 +38,31 @@ const Watch: React.FC<CmnProps<any>> = ({className='', xtraCss='', callBackFunc=
             callBackFunc(renderSideBar);
         };
     }, []);
-
+    
     if (!videoId) {
         navigate(-1);
         return <></>;
     };
 
-    const publishData = {source: demoVideoDetail as VideoDetailApiResponse , isLoading: false}
-  
-
-    // 2 components - Cinema & RelatedVids
-
-    // watch-yt flex-grow-[1] max-h-dvh overflow-y-scroll lg:scrollbar-cstm bg-zinc-900 flex justify-between flex-col lg:flex-row
     return (
         <div className={`watch-yt flex-grow-[1] max-h-dvh overflow-y-scroll scrollbar-hide lg:scrollbar-cstm bg-zinc-900 flex justify-between flex-col lg:flex-row ${className} ${xtraCss}`}>
-            <VideoContent className='min-h-[30%] lg:h-full w-full lg:w-[65%] flex flex-col px-2'
-                videoId={videoId} 
-                children={
-                    <PublisherData xtraCss='hidden lg:flex' source={publishData.source}
-                    isLoading={publishData.isLoading} /> 
-                }
-                isLoading={publishData.isLoading}  
-            />
-            <RelatedContent className='flex-grow-[1] max-h-dvh overflow-y-scroll sm:hide scrollbar-hide lg:scrollbar-cstm lg:h-full w-full lg:w-[34%]'
-                children={
-                    <PublisherData className=' lg:hidden' source={publishData.source} isLoading={publishData.isLoading} />
-                }
-                
-              />
+            <VideoContent className='min-h-[30%] lg:h-full w-full lg:w-[65%] flex flex-col px-2' videoId={videoId} isLoading={publishData.isLoading} >
+                <PublisherData xtraCss='hidden lg:flex' source={publishData.data} isLoading={publishData.isLoading} /> 
+            </VideoContent>    
+ 
+            <Routes>
+
+                <Route path='comments/*' element={ <div className='flex-grow-[1] max-h-dvh overflow-y-scroll sm:hide scrollbar-hide lg:scrollbar-cstm lg:h-full w-full lg:w-[34%]'> Comments Section </div> }/>
+                <Route path={'description/'} element={  <VidDescription className='flex-grow-[1] max-h-dvh overflow-y-scroll sm:hide scrollbar-hide lg:scrollbar-cstm lg:h-full w-full lg:w-[34%]'/> }/>
+                <Route path='*' element={ 
+                    <RelatedContent className='flex-grow-[1] max-h-dvh overflow-y-scroll sm:hide scrollbar-hide lg:scrollbar-cstm lg:h-full w-full lg:w-[34%]'>
+                        <PublisherData className=' lg:hidden' source={publishData.data} isLoading={publishData.isLoading} /> 
+                    </RelatedContent>
+                }/>
+
+            </Routes>
+  
+
         </div>
     );
 };
