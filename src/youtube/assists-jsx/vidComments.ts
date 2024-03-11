3b39a169d279474264b662fecfcd623aeef6e142
SubhPB <harvirbuttar2917@gmail.com>
2024-03-10 17:12:09 -0700

    Typescript for Comment Section

--- src/youtube/assists-jsx/vidComments.ts
+++ src/youtube/assists-jsx/vidComments.ts
@@ -1,57 +1,53 @@
-export interface YouTubeCommentsData {
-    comments: Comment[];
-    cursorNext: string;
-    filters: Filter[];
-    totalCommentsCount: number;
-}
+export interface Avatar {
+    height: number;
+    url: string;
+    width: number;
+  }
   
-export interface Comment {
-    author: Author;
-    commentId: string;
-    content: string;
-    creatorHeart: boolean;
-    cursorReplies?: string;
-    pinned: Pinned;
-    publishedTimeText: string;
-    stats: Stats;
-}
-
-export interface Author {
+export   interface Badge {
+    text: string;
+    type: string;
+  }
+  
+export   interface Author {
     avatar: Avatar[];
     badges: Badge[];
     channelId: string;
     isChannelOwner: boolean;
     title: string;
-}
-
-export interface Avatar {
-    height: number;
-    url: string;
-    width: number;
-}
-
-export interface Badge {
-    text?: string;
-    type?: string;
-}
-
-export interface Pinned {
+  }
+  
+export   interface Pinned {
     status: boolean;
-    text: null | string;
-}
-
-export interface Stats {
+    text: string | null;
+  }
+  
+export   interface Stats {
     replies: number;
     votes: number;
-}
-
-export interface Filter {
+  }
+  
+export   interface Comment {
+    author: Author;
+    commentId: string;
+    content: string;
+    creatorHeart: boolean;
+    cursorReplies: string | null;
+    pinned: Pinned;
+    publishedTimeText: string;
+    stats: Stats;
+  }
+  
+export   interface CommentsSection {
+    comments: Comment[];
+    cursorNext: string;
+    filters: Filter[];
+    totalCommentsCount: number;
+  }
+  
+export   interface Filter {
     cursorFilter: string;
     selected: boolean;
     title: string;
-}
-  
-
-
-
+  }
   
