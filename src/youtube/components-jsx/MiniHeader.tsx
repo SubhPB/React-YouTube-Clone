73f3673f9a1a0e7776251f5662e3ee5fc8768f71
SubhPB <harvirbuttar2917@gmail.com>
2024-03-14 15:39:25 -0700

    N/A

--- src/youtube/components-jsx/MiniHeader.tsx
+++ src/youtube/components-jsx/MiniHeader.tsx
@@ -12,28 +12,31 @@ function MiniHeader({className=' ', xtraCss='', children} : CmnProps<any>) {
   )
 };
 
-export const Genre: React.FC<CmnProps<string>> = ({className='py-2 px-3 font-semibold text-xs bg-zinc-600 rounded-[10px] cursor-pointer snap-start ', xtraCss='', source='Byimaan', url='/query/', children, loading}) => {
+export const Genre: React.FC<CmnProps<string>> = ({className='py-2 px-3 font-semibold text-xs bg-zinc-600 rounded-[10px] cursor-pointer snap-start ', xtraCss='', source='Byimaan', url, children, loading, handleClick}) => {
 
     const navigate = useNavigate();
 
     if (!source) return <></>;
     if (loading) return <div className={`genre ${className} ${xtraCss} whitespace-nowrap min-h-7 min-w-10`}>  </div>
 
-    const handleClick = () => { navigate(url + source ) };
+    const clicked = () => { 
+      if (url) navigate(url + source );
+      if (handleClick) handleClick();
+    };
 
     return (
-        <div onClick={handleClick} className={`genre ${className} ${xtraCss} whitespace-nowrap`}> {children} {source} </div>
+        <div onClick={clicked} className={`genre ${className} ${xtraCss} whitespace-nowrap`}> {children} {source} </div>
     );
 };
 
-export const Genres: React.FC<CmnProps<string[]>> = ({className='fixed z-[5] flex bg-zinc-900 p-2 gap-2 flex-shrink-0 w-full overflow-x-scroll scrollbar-hide ', xtraCss='', source, loading}) => {
+export const Genres: React.FC<CmnProps<string[]>> = ({className='fixed z-[5] flex bg-zinc-900 p-2 gap-2 flex-shrink-0 w-full overflow-x-scroll scrollbar-hide ', xtraCss='', source, loading, url}) => {
 
     if (!source || source?.length === 0 ) return <></>;
 
     return (
         <div className={`genres snap-x snap-mandatory ${className} ${xtraCss}`}>
             {
-                source?.map( (genre, index) => <Genre key={index} source={genre} loading={loading}/>)
+                source?.map( (genre, index) => <Genre key={index} source={genre} loading={loading} url={url}/>)
             }
         </div>
     )
---
+++ src/youtube/pages-jsx/channel-YT/components/ChannelCommunity.tsx
@@ -0,0 +1,13 @@
+/* -- BYIMAAN -> THE FUTURE -- */
+
+import React from 'react'
+import { ChnlProp } from '../ChannelTS';
+
+export const ChnlCommunity:React.FC<ChnlProp<any>> = ({className='', xtraCss=''}) => {
+
+    return (
+        <div className="">
+            This is the Community interface...
+        </div>
+    )
+};
