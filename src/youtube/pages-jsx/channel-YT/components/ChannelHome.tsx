a3cec4ce12613fe50da10ed5575e45fddb2522c7
SubhPB <harvirbuttar2917@gmail.com>
2024-03-14 12:00:06 -0700

    added sub-routes of channel -> home, playlist, community

---
+++ src/youtube/pages-jsx/channel-YT/components/ChannelHome.tsx
@@ -0,0 +1,11 @@
+/* -- BYIMAAN -> THE FUTURE -- */
+
+import React from 'react'
+import { ChnlProp } from '../ChannelTS';
+
+export const ChnlHome:React.FC<ChnlProp<any>> = ({className='', xtraCss=''}) => {
+
+    return (
+        <div className=""> This is the home interface... </div>
+    )
+}
