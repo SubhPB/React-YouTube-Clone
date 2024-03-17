6252fe80f47fca6d4de70f966af5dff6b1cf56c8
SubhPB <harvirbuttar2917@gmail.com>
2024-03-16 19:44:35 -0700

    n/a

--- src/youtube/pages-jsx/watch-YT/VideoButtons.tsx
+++ src/youtube/pages-jsx/watch-YT/VideoButtons.tsx
@@ -1,34 +1,21 @@
 /* -- BYIMAAN -> THE FUTURE -- */
 
 import React, { ReactElement } from 'react';
-import { AiOutlineLike } from "react-icons/ai";
-import { AiOutlineDislike } from "react-icons/ai";
-import { FaRegShareSquare } from "react-icons/fa";
-import { FaDownload } from "react-icons/fa6";
-import { FaReact } from "react-icons/fa";
 
-interface videoBtnTS {
-    text : string, icon : ReactElement
+export interface videoBtnTS {
+    text : string, icon : ReactElement, onClick ?: Function
 };
 
+export function VideoBtn( {text, icon, onClick} : videoBtnTS ): ReactElement {
 
-
-const videoBtns: videoBtnTS[] = [
-    {text: '', icon: <AiOutlineLike /> },
-    {text: '', icon: <AiOutlineDislike />},
-    {text: 'Share', icon: <FaRegShareSquare />},
-    {text: 'Download', icon: <FaDownload />},
-    {text: 'Byimaan', icon: <FaReact /> }
-];
-
-function VideoBtn( {text, icon} : videoBtnTS ): ReactElement {
+    const handleClick = () => {
+        if (onClick){ onClick() };
+    } 
 
     return (
-        <div className="video-btn flex jusitfy-around gap-2 items-center text-xs font-semibold">
+        <div onClick={ handleClick } className="video-btn flex jusitfy-around gap-2 items-center text-xs font-semibold">
             {icon}
             {text}
         </div>
     )
 };
-
-export const videoButtons: ReactElement[] = videoBtns.map( (data: videoBtnTS, index: number) => <VideoBtn key={index} text={data?.text} icon={data?.icon} /> )
