59d32ebfb186e53835150b01f9d14abe5c98d166
SubhPB <harvirbuttar2917@gmail.com>
2024-03-19 11:10:52 -0700

    have done debugging ...

--- src/youtube/pages-jsx/channel-YT/Channel.tsx
+++ src/youtube/pages-jsx/channel-YT/Channel.tsx
@@ -1,41 +1,41 @@
 /* -- BYIMAAN -> THE FUTURE -- */
 
-import React, { ReactElement, useState} from 'react';
+import React, { ReactElement, useState } from 'react';
 import { ChnlProp } from './ChannelTS';
 import { Data } from './ChannelTS';
 import { data } from './ChannelTS';
-import ChannelHeader from './components/ChannelHeader';
+import MemoChannelHeader from './components/ChannelHeader';
 import { ChnlHome } from './components/ChannelHome';
 import { ChnlCommunity } from './components/ChannelCommunity';
-import { Routes, Route } from 'react-router-dom';
-import { ChannelSearch } from './components/ChannelSearch';
+import { Routes, Route, useNavigate, useParams } from 'react-router-dom';
+import ChnlSearch from './components/ChannelSearch';
+import { IoSearch } from "react-icons/io5";
 
 
-type HOME = 'HOME'; type COMMUNITY = 'COMMUNITY';
-type RouteType = HOME | COMMUNITY
+
+type HOME = 'HOME'; type COMMUNITY = 'COMMUNITY'; type SEARCH = 'SEARCH';
+type RouteType = HOME | COMMUNITY | SEARCH;
 
 function Channel({className='', xtraCss='',}: ChnlProp<null>): ReactElement {
 
+    // why we using this useState (routeState):
+    //      to avoid the unnecessary rerendering of the same UI with same data.
+    //      Suppose if user first click to community. while staying at the Community we reclicked on Community
     const [routeState, setRouteState] = useState<RouteType>("HOME");
     const channelDetails = data as Data, isLoading = false;
 
-    function ChnlBody(): React.ReactElement {
-        switch (routeState) {
-            case 'HOME': return <ChnlHome ChnlData={data}/>;
-            case 'COMMUNITY': return <ChnlCommunity />;
-            default: return <ChnlHome />;
-        };
-    };
-
-
     return (
         <div className={`flex-grow-[1] max-h-dvh overflow-y-scroll lg:scrollbar-cstm p-2 ${className} ${xtraCss}`}>
-            <ChannelHeader src={channelDetails} isLoading={isLoading}/>
+            <MemoChannelHeader src={channelDetails} isLoading={isLoading}/>
             <SwitchRoute callBackFunc={() => [routeState, setRouteState]}/>
+
             <Routes>
-                <Route path='*' element={<ChnlBody />} /> 
-                <Route path='search/:searchQuery' element={<ChannelSearch />}/>
-            </Routes>    
+                <Route path='/' element={<ChnlHome/>}/>
+                <Route path='/home' element={<ChnlHome/>}/>
+                <Route path='/community' element={<ChnlCommunity/>}/>
+                <Route path='*' element={<ChnlHome/>}/>
+                <Route path='search/:searchQuery' element={<ChnlSearch />}/>
+            </Routes>
         </div>
     );
 };
@@ -43,17 +43,53 @@ function Channel({className='', xtraCss='',}: ChnlProp<null>): ReactElement {
 function SwitchRoute ({src=['HOME', 'COMMUNITY'], isLoading, callBackFunc= () => [] }:ChnlProp<RouteType[]>) {
 
     const [currRoute, setRoute] = callBackFunc();
+    const navigate = useNavigate();
+    const { channelId } = useParams();
+
+    function Navigator( requestedUrl : string, futureRouteState: HOME | COMMUNITY | SEARCH, forcefully: boolean = false ){
+        if ( !(futureRouteState === currRoute) || forcefully ){
+            navigate(requestedUrl);
+            setRoute(futureRouteState)
+        };
+    };
+
+    function _SearchInput(){
+
+        const [searchQuery, setSearchQuery] = useState<string>('');
+
+        const handleChange: React.ChangeEventHandler<HTMLInputElement> = ( e ) => {
+            setSearchQuery(e?.target?.value);
+        };
+        const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
+            
+            if (searchQuery.trim().length !== 0 && e.key === 'Enter'){
+                Navigator(`/channel/${channelId}/search/${searchQuery.trim()}`, 'SEARCH', true);
+                setSearchQuery('');
+            };
+        };
+
+        return (
+            <div className="search-input flex items-center gap-2">
+                <IoSearch />
+                <input onChange={handleChange} value={searchQuery}
+                  className='outline-none border-b-[1px] border-zinc-300 pb-1 text-white bg-transparent'
+                  type='text' onKeyDown={handleKeyDown}
+                />
+            </div>
+        )
+    };
 
     return (
         <div className="w-full flex gap-4 my-1 overflow-x-scroll scrollbar-hide">
             {
                 src?.map( (route, ind) => (
-                    <p onClick={ () => setRoute(route) } key={ind} 
+                    <p onClick={ () => Navigator(`/channel/${channelId}/${route.toLowerCase()}`, route) } key={ind} 
                         className={`text-md p-2 semibold cursor-default ${route === currRoute ? 'border-b-white border-b-[2px] scale-[1]' : '' }`}>
                         {route[0] + route.slice(1).toLowerCase()}
                     </p>) 
                 )
             }
+            <_SearchInput />
         </div>
     )
 };
