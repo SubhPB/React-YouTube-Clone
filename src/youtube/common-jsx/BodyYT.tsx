5f2ba32c60b82cd01380fb942865c40eb2c1db00
SubhPB <harvirbuttar2917@gmail.com>
2024-03-20 10:16:59 -0700

    n/a

--- src/youtube/common-jsx/BodyYT.tsx
+++ src/youtube/common-jsx/BodyYT.tsx
@@ -30,9 +30,12 @@ function BodyYT() {
   return (
     <div className='body-yt flex-grow-[1] max-h-dvh overflow-y-scroll lg:scrollbar-cstm '>
 
-      {<MiniHeader>
-        <Genres source={ data?.refinements} url={'/query/'}/>
-      </MiniHeader>  }
+      { 
+        data?.refinements && (
+        <MiniHeader>
+          <Genres source={ data?.refinements} url={'/query/'}/>
+        </MiniHeader> )
+      }
 
       { (q !== 'home' && data?.didYouMean ) && <DidYouMean text={data?.didYouMean ?? null} />}
 
