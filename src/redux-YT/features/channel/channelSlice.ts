08ea3fee6d5cf5e36444b6c4cf67861ea91d12e3
SubhPB <harvirbuttar2917@gmail.com>
2024-03-22 08:35:05 -0700

    channel slice & and actions

---
+++ src/redux-YT/features/channel/channelSlice.ts
@@ -0,0 +1,9 @@
+/* -- BYIMAAN -> THE FUTURE -- */
+
+import { Data } from "../../../youtube/pages-jsx/channel-YT/ChannelTS";
+
+import { createCustomSlice } from "../api/apiSlice";
+
+// const { slice: channelSlice, sliceActions: channelActions , sliceReducer: channelreducer  } = createCustomSlice('channel');
+
+export default ''
