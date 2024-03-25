9ced2ec0a1cc94ed3cdb07a691bdb970f8cd34d7
SubhPB <harvirbuttar2917@gmail.com>
2024-03-25 08:20:06 -0700

    implemented fetching logic for home data.

--- src/redux-YT/features/api/apiSlice.ts
+++ src/redux-YT/features/api/apiSlice.ts
@@ -3,8 +3,81 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { apiFetchTS } from "./apiTypes";
 
-const intitalApiState: apiFetchTS<string> = {
+const initialApiState: apiFetchTS<any> = {
     data: null, error: null, isLoading: true
 };
 
-//  will continue to work on slice. soon ....
+// const ApiSlice = createSlice({
+//     name: 'api',
+//     initialState: initialApiState,
+//     reducers: {
+//         fetchApiStart(state) {
+//             state = {
+//                 ...state,
+//                 data: null,
+//                 isLoading: true,
+//                 error: null,
+//             };
+//         }, 
+//         fetchDataSuccess<T>(state: apiFetchTS<any>, action: PayloadAction<T>){
+//             state = {
+//                 ...state,
+//                 data: action.payload,
+//                 isLoading: false,
+//                 error: null
+//             };
+//         },
+//         fetchDataError(state, action: PayloadAction<string>){
+//             state = {
+//                 ...state, 
+//                 data: null,
+//                 isLoading: false,
+//                 error: action.payload
+//             };
+//         },
+//     },
+// });
+
+export const createCustomSlice = (sliceName : string) => {
+    const slice =  createSlice({
+        name: sliceName,
+        initialState: initialApiState,
+        reducers: {
+            fetchApiStart(state) {
+                state = {
+                    ...state,
+                    data: null,
+                    isLoading: true,
+                    error: null,
+                };
+            }, 
+            fetchDataSuccess<T>(state: apiFetchTS<any>, action: PayloadAction<T>){
+                state = {
+                    ...state,
+                    data: action.payload,
+                    isLoading: false,
+                    error: null
+                };
+            },
+            fetchDataError(state, action: PayloadAction<string>){
+                state = {
+                    ...state, 
+                    data: null,
+                    isLoading: false,
+                    error: action.payload
+                };
+            },
+        },
+    });
+
+    const sliceActions = slice.actions;
+
+    const sliceReducer = slice.reducer;
+
+    return [slice, sliceActions, sliceReducer]
+};
+
+
+
+// export const {fetchApiStart, fetchDataSuccess} = ApiSlice.actions;
+// export default ApiSlice.reducer;
--- src/redux-YT/features/api/apiTypes.ts
+++ src/redux-YT/features/api/apiTypes.ts
@@ -1,7 +1,29 @@
 /* -- BYIMAAN -> THE FUTURE -- */
+import { Action } from "redux"
 
 export interface apiFetchTS<T> {
     data: T | null,
     error: string | null,
     isLoading: boolean
 };
+
+export enum ApiStateTypes {
+    FETCH_DATA_START = 'FETCH_DATA_START',
+    FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS',
+    FETCH_DATA_ERROR = 'FETCH_DATA_ERROR'
+};
+
+interface FetchDataStartAction extends Action {
+    type: ApiStateTypes.FETCH_DATA_START;
+};
+
+interface FetchDataSuccessAction<T> extends Action{
+    type: ApiStateTypes.FETCH_DATA_SUCCESS;
+    payload: T
+};
+
+interface FetchDataErrorAction extends Action {
+    type: ApiStateTypes.FETCH_DATA_ERROR
+};
+
+export type ApiActions<T> = FetchDataStartAction | FetchDataErrorAction | FetchDataSuccessAction<T>;
