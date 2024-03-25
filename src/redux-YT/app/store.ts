220deeeebfd863c4bf6a677156a297b548cd982e
SubhPB <harvirbuttar2917@gmail.com>
2024-03-25 08:18:10 -0700

    added channel reducer

--- src/redux-YT/app/store.ts
+++ src/redux-YT/app/store.ts
@@ -1,7 +1,22 @@
 /* -- BYIMAAN -> THE FUTURE -- */
 
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, Action, ThunkAction } from "@reduxjs/toolkit";
+import rootReducer from "./rootReducer";
+import { useDispatch } from "react-redux";
 
 export const store = configureStore({
-    reducer: {},
+    reducer: rootReducer,
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
+>
