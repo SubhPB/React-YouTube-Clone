c62bbcc4ce397d67e5d557ba432a57b1f62164ce
SubhPB <harvirbuttar2917@gmail.com>
2024-03-25 15:01:36 -0700

    added new video-reducer

--- src/redux-YT/app/rootReducer.ts
+++ src/redux-YT/app/rootReducer.ts
@@ -3,10 +3,12 @@
 import { combineReducers } from "redux";
 import HomeReducer from "../features/home/homeSlice";
 import { ChannelReducer } from "../features/channel/channelReducer";
+import { VideoReducer } from "../features/video/videoReducer";
 
 const rootReducer = combineReducers({
     home : HomeReducer,
     channel: ChannelReducer,
+    video: VideoReducer,
 });
 
 export default rootReducer;
