d86c4a1f03ce099b9e2fca9477454214aebbee22
SubhPB <harvirbuttar2917@gmail.com>
2024-03-27 10:13:58 -0700

    thunk for fetching channel data

--- src/redux-YT/features/channel/channelReducer.ts
+++ src/redux-YT/features/channel/channelReducer.ts
@@ -2,8 +2,13 @@
 
 import { combineReducers } from "redux";
 import ChnlCommunityReducer from "./chnlCommunity/communitySlice";
-
+import ChnlHomeReducer from "./chnlHome/homeSlice";
+import ChnlSearchReducer from "./chnlSearch/searchSlice";
+import ChnlDetailsReducer from "./chnlDetails/detailSlice";
 
 export const ChannelReducer = combineReducers({
+    home: ChnlHomeReducer,
     community: ChnlCommunityReducer,
+    search: ChnlSearchReducer,
+    details: ChnlDetailsReducer
 });
