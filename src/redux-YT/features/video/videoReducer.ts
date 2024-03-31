9cf2622f458c7a42069bffb2460cdb1cc9611315
SubhPB <harvirbuttar2917@gmail.com>
2024-03-31 03:22:53 -0700

    video reducer....

--- src/redux-YT/features/video/videoReducer.ts
+++ src/redux-YT/features/video/videoReducer.ts
@@ -2,7 +2,9 @@
 
 import { combineReducers } from "redux";
 import CommentsReducer from './comments/commentsSlice';
+import TargetVideosReducer from "./targetVideos/targetVidSlice";
 
 export const VideoReducer = combineReducers({
     comments: CommentsReducer,
-})
+    targetVideos: TargetVideosReducer,
+});
