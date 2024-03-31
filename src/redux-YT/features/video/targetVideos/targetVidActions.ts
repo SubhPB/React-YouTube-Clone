b620f26d516cc6d77c269eadf6774e1adcc6dd16
SubhPB <harvirbuttar2917@gmail.com>
2024-03-31 03:13:06 -0700

    action to like dilike videi]o

--- src/redux-YT/features/video/targetVideos/targetVidActions.ts
+++ src/redux-YT/features/video/targetVideos/targetVidActions.ts
@@ -8,17 +8,43 @@ import { StatusTs } from "./targetVidTypes";
 
 function likeVideoAction(state: VideoDataTs, action: PayloadAction<IdActionType> ) {
     const videoId = action.payload.id;
-    state[videoId].status =  StatusTs.LIKED
+    const videoExist = Object.keys(state).includes(videoId);
+    if (videoExist){
+        state[videoId].status = StatusTs.LIKED;
+    } else {
+        state[videoId] = {
+            status : StatusTs.LIKED,
+            myComments: [],
+        }
+    }
 };
 
 function dislikeVideoAction(state: VideoDataTs, action: PayloadAction<IdActionType> ) {
     const videoId = action.payload.id;
-    state[videoId].status =  StatusTs.DISLIKE;
+    const videoExist = Object.keys(state).includes(videoId);
+    if (videoExist){
+        state[videoId].status = StatusTs.DISLIKE;
+    } else {
+        state[videoId] = {
+            status : StatusTs.DISLIKE,
+            myComments: [],
+        }
+    }
 };
 
 function neutralVideoAction(state: VideoDataTs, action: PayloadAction<IdActionType> ) {
     const videoId = action.payload.id;
-    state[videoId].status =  StatusTs.NEUTRAL;
+    const videoExist = Object.keys(state).includes(videoId);
+    if (videoExist){
+        // if videoExists & it's state going to be neutral & do not have comments, there is no sense to save it in state.
+        const haveComments = state[videoId].myComments.length > 0
+        haveComments ? state[videoId].status = StatusTs.NEUTRAL : delete state[videoId]
+    } else {
+        state[videoId] = {
+            status : StatusTs.NEUTRAL,
+            myComments: [],
+        }
+    }
 };
 
 
