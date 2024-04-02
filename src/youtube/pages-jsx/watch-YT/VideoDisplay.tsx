c1bf79cd97e9de9c8f28ed6ef5b5e8ea382734f8
SubhPB <harvirbuttar2917@gmail.com>
2024-04-02 09:08:20 -0700

    uncommented component! for some time

--- src/youtube/pages-jsx/watch-YT/VideoDisplay.tsx
+++ src/youtube/pages-jsx/watch-YT/VideoDisplay.tsx
@@ -3,23 +3,25 @@
 import React from 'react';
 import { vidCardTS } from '../../../src-utils/dataTypes';
 import { useParams } from 'react-router-dom';
-import useFetch from '../../../src-utils/useFetch';
 import ReactPlayer from 'react-player';
 
 function VideoDisplay({className='', xtraCss='', source, isLoading=false }: vidCardTS): React.ReactElement {
   const {videoId} = useParams();
   if (isLoading) return <div className={`load-vid-display ${className} ${xtraCss}`}> <div className=' h-full w-full bg-zinc-600 rounded-lg'/></div>
 
-  return (
-    <div className={`vid-display relative ${className} ${xtraCss} `}>
-      <ReactPlayer 
-      url={`https://www.youtube.com/watch?v=${videoId}`}
-      className={'absolute top-0 left-0'}
-      width={'100%'} height={'100%'} playing 
-      controls={true}
-      />
-    </div>
-  )
+  
+  return  <>{
+      videoId ?
+      <div className={`vid-display relative ${className} ${xtraCss} `}>
+        {/* <ReactPlayer 
+        url={`https://www.youtube.com/watch?v=${videoId}`}
+        className={'absolute top-0 left-0'}
+        width={'100%'} height={'100%'} playing={false}
+        controls={true} 
+        /> */}
+      </div> : <></>
+    } </>
+  
 }
 
 export default VideoDisplay
