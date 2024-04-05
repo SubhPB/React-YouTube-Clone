d030c8ee3da39b905707cf68c8f87322dc37ce51
SubhPB <harvirbuttar2917@gmail.com>
2024-04-05 10:07:19 -0700

    ...

--- src/redux-YT/app/rootReducer.ts
+++ src/redux-YT/app/rootReducer.ts
@@ -4,11 +4,13 @@ import { combineReducers } from "redux";
 import HomeReducer from "../features/home/homeSlice";
 import { ChannelReducer } from "../features/channel/channelReducer";
 import { VideoReducer } from "../features/video/videoReducer";
+import { HistoryReducer } from "../features/history/slice";
 
 const rootReducer = combineReducers({
     home : HomeReducer,
     channel: ChannelReducer,
     video: VideoReducer,
+    history: HistoryReducer,
 });
 
 export default rootReducer;
