1f927655ee199378454e2fc86722beab4603a3ba
SubhPB <harvirbuttar2917@gmail.com>
2024-04-06 20:32:35 -0700

    route for subcription

--- src/youtube/common-jsx/MenuYT.tsx
+++ src/youtube/common-jsx/MenuYT.tsx
@@ -7,30 +7,35 @@ import { MdSubscriptions } from "react-icons/md";
 import { FaAngleRight } from "react-icons/fa6";
 import { menuData } from '../assists-jsx/data';
 import { useNavigate } from 'react-router-dom';
+import { _MenuItem } from '../assists-jsx/data';
 
 
 const MenuYT: React.FC<{xtraCss?: string}> = ({xtraCss=''}) => {
 
     const navigate = useNavigate();
 
-    const migrate = (url: string) => {
-        navigate(url);  
-    };
+    const handleClick = (menuItem: _MenuItem ) => {
+        if (menuItem?.url) {
+            navigate(menuItem.url);
+        } else if (menuItem?.onClick){
+            menuItem.onClick();
+        }
+    }
 
     return (
         <div className={'absolute h-[93.6vh] lg:h-[91vh] min-w-[22vmax] bg-zinc-900 top-[107%] left-[0%] overflow-scroll scrollbar-hide z-[10]'+ xtraCss}>
             <div className="top-menu-items w-full py-2">
                 <Section name='General'>
-                    <MenuItem component={<AiFillHome />} name='Home'/>
+                    <MenuItem component={<AiFillHome />} name='Home' onClick={() => navigate('/')}/>
                     <MenuItem component={<SiYoutubeshorts />} name='Shorts'/>
-                    <MenuItem component={<MdSubscriptions />} name='Subscription'/>
+                    <MenuItem component={<MdSubscriptions />} name='Subscription' onClick={() => navigate('/user/general/subcriptions')}/>
                 </Section>
                 {
                     menuData.map( (section, secInd) => (
                         <Section key={secInd} name={section.sectionName}>
                             {
                                 section.menuItems.map( (menuItem, itemInd) => (
-                                    <MenuItem key={`${menuItem.itemName}-&-${itemInd}`} component={menuItem.component} name={menuItem.itemName} handleClick={migrate} parentName={section.sectionName}/>
+                                    <MenuItem key={`${menuItem.itemName}-&-${itemInd}`} component={menuItem.component} name={menuItem.itemName} parentName={section.sectionName} onClick={() => handleClick(menuItem)}/>
                                 ))
                             }
                         </Section>
@@ -44,20 +49,15 @@ const MenuYT: React.FC<{xtraCss?: string}> = ({xtraCss=''}) => {
 interface Prop {
     className ?: string, xtraCss ?: string,
     component ?: ReactElement, name ?: String | undefined,
-    children ?: ReactNode, handleClick ?: (url: string) => void,
+    children ?: ReactNode, onClick ?: Function,
     parentName ?: string,
 };
 
 
-const MenuItem: React.FC<Prop> = ({className=' ', xtraCss=' ',component, name, handleClick= () => {}, parentName}) => {
-
-    const handleNavigation = () => {
-        
-        handleClick(`/${parentName ? parentName?.toLowerCase() + '/' + name?.toLowerCase() : name?.toLowerCase()}`)
-    }
+const MenuItem: React.FC<Prop> = ({className=' ', xtraCss=' ',onClick= () => {},component, name}) => {
 
     return (
-        <div className={`menu-item flex items-center px-2 gap-2 py-1 cursor-pointer hover:bg-zinc-500 rounded-[14px]  ${className} ${xtraCss}`} onClick={handleNavigation}>
+        <div onClick={() => onClick()} className={`menu-item flex items-center px-2 gap-2 py-1 cursor-pointer hover:bg-zinc-500 rounded-[14px]  ${className} ${xtraCss}`}>
             {component}
             <p className='text-[15px] font-semibold'>{name}</p>
         </div>
