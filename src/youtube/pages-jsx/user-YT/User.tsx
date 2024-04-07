42704169dbb5afc7ea8f5b98661205a25f3f6a13
SubhPB <harvirbuttar2917@gmail.com>
2024-04-07 05:06:42 -0700

    added route for subcription

--- src/youtube/pages-jsx/user-YT/User.tsx
+++ src/youtube/pages-jsx/user-YT/User.tsx
@@ -3,6 +3,8 @@
 import React from 'react';
 import LikedVideos from './you-page/LikedVideo';
 import { Route, Routes } from 'react-router-dom';
+import History from './you-page/History';
+import Subcriptions from './general-page/Subcriptions';
 
 export interface UserProp {
     className ?: string,
@@ -13,9 +15,11 @@ export interface UserProp {
 
 function UserYT({className='', xtraCss=''}: UserProp) {
   return (
-    <div className={`user-yt flex-grow-[1] max-h-dvh overflow-y-scroll lg:scrollbar-cstm bg-zinc-300 ${className} ${xtraCss}`}>
+    <div className={`user-yt flex-grow-[1] max-h-dvh overflow-y-scroll lg:scrollbar-cstm ${className} ${xtraCss}`}>
       <Routes>
         <Route path='you/liked-videos' element={<LikedVideos />}/>
+        <Route path='you/history' element={<History />} />
+        <Route path='general/subcriptions' element={<Subcriptions />} />
       </Routes>
     </div>
   )
