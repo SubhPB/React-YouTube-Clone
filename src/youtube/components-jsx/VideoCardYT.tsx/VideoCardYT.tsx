e8aeb15440e8fa2ad5c2b8e82a7480fdd9c61906
SubhPB <harvirbuttar2917@gmail.com>
2024-04-08 09:02:33 -0700

    -fixed css bugs in video card-

--- src/youtube/components-jsx/VideoCardYT.tsx/VideoCardYT.tsx
+++ src/youtube/components-jsx/VideoCardYT.tsx/VideoCardYT.tsx
@@ -8,22 +8,35 @@ import { _ChannelImg } from "./VideoDescription";
 import { _CardDetails } from "./VideoDescription";
 import { _CardTitle } from "./VideoDescription";
 import { _CardChannelName } from "./VideoDescription";
+import { useAppDispatch } from "../../../redux-YT/app/store";
+import { appendVideoInHistory } from "../../../redux-YT/features/history/slice";
 
 export const VideoCardYT: React.FC<vidCardTS> = ({className='', xtraCss='', source=null, isLoading=true, isSmall=false}) => {
 
     const ifSmallSize = {
-        videoCardCss: 'min-h-[18vh] w-full my-1 cursor-default flex justify-between',
-        thumbnailCss: ' h-full w-[25vh] lg:w-[30vh] mr-1 flex-shrink-[0] ',
+        videoCardCss: 'min-h-[18vh] max-h-[19vh] w-full my-1 cursor-default flex justify-between',
+        thumbnailCss: ' h-full w-[21vh] lg:w-[30vh] mr-1 flex-shrink-[0] ',
         videoDescCss: 'flex-grow-[1] ',
-        cardDetailsCss: 'w-full h-full flex flex-col pb-4 gap-2 justify-between ',
+        cardDetailsCss: 'w-full h-full flex flex-col pb-4 gap-1  ',
+    };
+
+    const appDispatch = useAppDispatch();
+
+    const saveInHistory = () => {
+        if (source) {
+            appDispatch( appendVideoInHistory({
+                type: 'video',
+                video: source
+            }))
+        };
     };
 
     return (
-        <div className={isSmall ? ifSmallSize.videoCardCss : "vid-card min-h-[38vh] lg:min-h-[44vh] w-full lg:w-1/3 p-4 mb-4 cursor-default"}>
+        <div className={isSmall ? ifSmallSize.videoCardCss : "vid-card min-h-[38vh] lg:min-h-[44vh] w-full lg:w-1/3 p-4 mb-4 cursor-default max-h-[40vh]"} onClick={saveInHistory}>
 
             <_Thumbnail className={ isSmall ? ifSmallSize.thumbnailCss : 'h-[85%] w-full mb-2' } isLoading={isLoading} source={source} />
 
-            <VideoDesc className={isSmall ? ifSmallSize.videoDescCss : 'flex flex-row gap-3 justify-around items-center w-full h-fit'} isLoading={isLoading} source={source}>
+            <VideoDesc className={isSmall ? ifSmallSize.videoDescCss : 'flex flex-row gap-2 justify-around items-center w-full h-fit'} isLoading={isLoading} source={source}>
                 
                 { !isSmall && <_ChannelImg className='h-11 min-w-11 max-w-11 rounded-full overflow-hidden' source={source} isLoading={isLoading} />}
 
