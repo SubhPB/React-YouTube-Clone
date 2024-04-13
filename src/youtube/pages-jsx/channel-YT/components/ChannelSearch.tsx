13b088d6b4c1d8fbbc1dd07c7320e38bf1e95785
SubhPB <harvirbuttar2917@gmail.com>
2024-04-13 08:19:12 -0700

    api-call by thunk to fetch query data

--- src/youtube/pages-jsx/channel-YT/components/ChannelSearch.tsx
+++ src/youtube/pages-jsx/channel-YT/components/ChannelSearch.tsx
@@ -1,6 +1,6 @@
 /* -- BYIMAAN -> THE FUTURE -- */
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ChnlProp } from '../ChannelTS';
 import { useParams } from 'react-router-dom';
 import useFetch from '../../../../src-utils/useFetch';
@@ -10,13 +10,27 @@ import FlexContainer from '../../../assists-jsx/FlexContainer';
 import { LoadingFlexContainer } from '../../../common-jsx/BodyYT';
 import { VideoCardYT } from '../../../components-jsx/VideoCardYT.tsx/VideoCardYT';
 
+import { useAppDispatch } from '../../../../redux-YT/app/store';
+import { chnlSearchStateSelector } from '../../../../redux-YT/features/channel/chnlSearch/searchSelectors';
+import { useSelector } from 'react-redux';
+import { fetchChnlSearchAction } from '../../../../redux-YT/features/channel/chnlSearch/searchActions';
 
 const ChannelSearch:React.FC<ChnlProp<any>> = ({className='', xtraCss=''}) => {
 
     const {channelId, searchQuery} = useParams();
 
     // const {data, error, isLoading} = useFetch<ApiResponse>(`channel/search/?id=${channelId}&q=${searchQuery}`)
-    const [data , error, isLoading] : [ApiResponse, null, boolean] = [searchDemoData as ApiResponse, null, false]
+    // const [data , error, isLoading] : [ApiResponse, null, boolean] = [searchDemoData as ApiResponse, null, false]
+
+    // thunk
+    const appDispatch = useAppDispatch();
+    const { data, error, isLoading} = useSelector( chnlSearchStateSelector );
+    
+    useEffect( function didMount(){
+
+        if (channelId && searchQuery) appDispatch( fetchChnlSearchAction(channelId, searchQuery) );
+
+    }, [appDispatch, channelId, searchQuery])
 
     if (isLoading) return <LoadingFlexContainer />
     return (
