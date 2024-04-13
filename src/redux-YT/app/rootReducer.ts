1484dda22fa7aaa1a477e816caec171e62543ca5
SubhPB <harvirbuttar2917@gmail.com>
2024-04-13 08:16:30 -0700

    added apiError reducer

--- src/redux-YT/app/rootReducer.ts
+++ src/redux-YT/app/rootReducer.ts
@@ -5,12 +5,14 @@ import HomeReducer from "../features/home/homeSlice";
 import { ChannelReducer } from "../features/channel/channelReducer";
 import { VideoReducer } from "../features/video/videoReducer";
 import { HistoryReducer } from "../features/history/slice";
+import ApiErrorReducer from '../features/error/slice';
 
 const rootReducer = combineReducers({
     home : HomeReducer,
     channel: ChannelReducer,
     video: VideoReducer,
     history: HistoryReducer,
+    apiError: ApiErrorReducer,
 });
 
 export default rootReducer;
