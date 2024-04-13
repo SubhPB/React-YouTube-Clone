f848a253369674c3ee882b3e224207be9a25f6a4
SubhPB <harvirbuttar2917@gmail.com>
2024-04-13 08:15:37 -0700

    ...

--- src/redux-YT/features/video/videoReducer.ts
+++ src/redux-YT/features/video/videoReducer.ts
@@ -4,9 +4,13 @@ import { combineReducers } from "redux";
 import CommentsReducer from './comments/commentsSlice';
 import TargetVideosReducer from "./targetVideos/targetVidSlice";
 import TargetCommentsReducer from "./comments/targetComments/slice";
+import VideoDetailsReducer from './details/slice' 
+import RelatedContentReducer from './related-content/slice';
 
 export const VideoReducer = combineReducers({
     comments: CommentsReducer,
+    details: VideoDetailsReducer,
+    relatedContent: RelatedContentReducer,
     targetVideos: TargetVideosReducer,
-    targetComments: TargetCommentsReducer 
+    targetComments: TargetCommentsReducer, 
 });
