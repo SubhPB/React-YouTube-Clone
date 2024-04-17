3eb51ba7396e1411bb9b19d5ae4ea4b18c7aac4d
SubhPB <harvirbuttar2917@gmail.com>
2024-04-17 11:54:09 -0700

    ...

--- src/redux-YT/features/home/homeSelectors.ts
+++ src/redux-YT/features/home/homeSelectors.ts
@@ -3,6 +3,6 @@
 import { createSelector } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
 
-export const selectHome = (state: RootState) => state.home;
+export const selectHome = (state: RootState) => state;
 
-export const homeStateSelector = createSelector( selectHome, state => state );
+export const homeStateSelector = createSelector( selectHome, state => state.home );
