2a60a8ec2c2d7cf310314a00c2e404d4125d122e
SubhPB <harvirbuttar2917@gmail.com>
2024-04-18 08:14:12 -0700

    changed logic about token-key

--- src/src-utils/api.ts
+++ src/src-utils/api.ts
@@ -4,7 +4,7 @@ import axios, {AxiosError} from "axios";
 import {obj, AsyncFunc} from './dataTypes';
 
 const BASE_URL = "https://youtube138.p.rapidapi.com";
-
+const token = localStorage.getItem('token');
 
 const options: obj<obj<string>> = {
   params: {
@@ -12,27 +12,61 @@ const options: obj<obj<string>> = {
     gl: 'US'
   },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_UTUBE_API_KEY!,
+    'X-RapidAPI-Key': token ?? 'null',
     'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
   }
 };
 
-export const fetchData: AsyncFunc<[string]> = async(url) => {
+interface AxiosErr extends AxiosError {
+  retry ?: boolean;
+};
+
+export const fetchData: AsyncFunc<[string]> = async(url) : Promise<any> => {
     // Imp: async func always returns a promise...
 
     try {
+      
+      if (!token) return Promise.reject(new Error('Token is missing, cannot make an api without key.'));
 
       const {data} = await axios.get(`${BASE_URL}/${url}`, options);
       return data;
 
     } catch (error){
-      const axiosErr = error as AxiosError 
-      console.log("The request made on " + `'${BASE_URL}/${url}'` + " got rejected.");
-      console.log(error);
+
+      const axiosErr = error as AxiosErr; 
+
+      // if (axios.isAxiosError(error)) {
+      //   const axiosErr = error as AxiosErr; 
+      //   if (axiosErr.response?.status === 429 && !axiosErr.retry){
+      //     return handleRateLimiting(axiosErr);
+      //   };
+      // };
+
       return Promise.reject(axiosErr);
 
     }
     
 };
+
+async function handleRateLimiting(error: AxiosErr) : Promise<any> {
+
+  if (!token) return Promise.reject(error);
+
+  try {
+    const config = {
+      ...error.config,
+      headers: {
+        ...error.config?.headers,
+        'X-RapidAPI-Key': token
+      }
+    };
+    error.retry = true;
+    const {data} = await axios(config);
+    return data;
+
+  } catch (retryErr) {
+    return Promise.reject(retryErr);
+  }
+}
 
 
