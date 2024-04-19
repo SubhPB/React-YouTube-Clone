bd682d5b361db320cd3f26ea8503dc62e62264ca
SubhPB <harvirbuttar2917@gmail.com>
2024-04-19 11:12:05 -0700

    ...

--- src/youtube/YouTube.tsx
+++ src/youtube/YouTube.tsx
@@ -1,7 +1,7 @@
 /* -- BYIMAAN -> THE FUTURE -- */
 
-import React, { useState, useEffect } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import DividerYT from './common-jsx/DividerYT';
 import HeaderYT from './common-jsx/HeaderYT';
 import SideBar from './common-jsx/SideBar';
@@ -10,21 +10,20 @@ import Watch from './pages-jsx/watch-YT/Watch';
 import Channel from './pages-jsx/channel-YT/Channel';
 import UserYT from './pages-jsx/user-YT/User';
 import { useSelector } from 'react-redux';
-import { selectApiError } from '../redux-YT/features/error/selector';
+import { selectToken } from '../redux-YT/features/api/selectors';
 import ApiError from './pages-jsx/error-api-YT/ApiError';
 
 function YouTube() {
 
   const [renderSideBar, setRenderSideBar] = useState<boolean>(true);
 
-  const apiError = useSelector(selectApiError);
-  const navigate = useNavigate();
+  const token = useSelector(selectToken);
+  const apiError = token === null;
 
-  console.log("Api-error ",apiError);
-  useEffect( function didUpdate() {
-    
-    if (apiError) navigate('/api-error');
-  }, [apiError]);
+  useEffect( 
+    function didUpdate() {
+      token ? localStorage.setItem('token', token) : localStorage.removeItem('token')
+  }, [token]);  
 
   return (
     <div className='y-tube w-[100vw] h-[100vh] pt-[0.8vmax] px-[0.8vmax] pb-[0.4vmax] overflow-y-hidden bg-zinc-900 flex flex-col gap-2'>
@@ -32,15 +31,21 @@ function YouTube() {
 
         <DividerYT>
 
-          <Routes>
-            <Route path='query/:q' element={<BodyYT />} />
-            <Route path='/' element={<BodyYT />}/>
-            <Route path='/watch/:videoId/*' element={<Watch callBackFunc={setRenderSideBar} />}/>
-            <Route path='/channel/:channelId/*' element={<Channel /> } />
-            <Route path='/user/*' element={<UserYT />}/>
-            {apiError && <Route path='/api-error' element={<ApiError />}/>}
-            <Route path='*' element={<> -N/A- not found ...</>} />
-          </Routes>
+        
+        { 
+          apiError ? <ApiError /> : (
+              <Routes>
+                <Route path='query/:q' element={<BodyYT    />} />
+                <Route path='/' element={<BodyYT  />}/>
+                <Route path='/watch/:videoId/*' element={<Watch   callBackFunc={setRenderSideBar} />}/>
+                <Route path='/channel/:channelId/*' element={<Channel   /> } />
+                <Route path='/user/*' element={<UserYT  />}/>
+                {apiError && <Route path='/api-error' element={<ApiError />}/>}
+                <Route path='*' element={<> -N/A- not found ...</>} />
+              </Routes>
+            )
+          }
+       
 
           { renderSideBar && <SideBar /> }
 
