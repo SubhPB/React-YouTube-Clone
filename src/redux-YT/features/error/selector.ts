bd688f231af7af99ee5a455bff98c3f3a198e4ce
SubhPB <harvirbuttar2917@gmail.com>
2024-04-19 08:24:31 -0700

    deleted

--- src/redux-YT/features/error/selector.ts
+++
@@ -1,9 +0,0 @@
-/* -- BYIMAAN -> THE FUTURE -- */
-import { createSelector } from "@reduxjs/toolkit";
-import { RootState } from "../../app/store";
-import { ApiErrorState } from "./slice";
-
-export const selectApiError = createSelector(
-    (state: RootState) => state.apiError,
-    (state: ApiErrorState) => state.state,
-);
